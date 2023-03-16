d75cf7bdf751a24db0ae589c524e4e3f070ccd81
SANJAIRAGUL-MURUGESAN <sanjairagulm@gmail.com>
2023-03-16 10:30:24 +0530

    Change in Header.js

--- Header.js
+++ Header.js
@@ -1,6 +1,9 @@
 const http = createServer((req,res)=>{
     // Setting Header for Response
     res.setHeader('content-type' , 'plain/text') 
+    res.writeHead(200,{
+        'content-type' : 'plain/text'
+    })
     if(req.method==='GET'){
         res.write('Here is Respose For Your Request')
         res.end()
